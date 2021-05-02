be30c3d1374d7d147cc3a41821bf40a6eabeb331
cshablin <Carmel.Shablin@kla-tencor.com>
2021-05-02 17:26:34 +0300

    minor test update

--- MetricTests.py
+++ MetricTests.py
@@ -42,26 +42,27 @@ class MyTestCase(unittest.TestCase):
 
     def test_maven_repo_generate_all_raw_metric(self):
         folder_with_commits_files = "C:\\My_Stuff\\BGU\\לימודים\\2021\\איתור תקלות\\פרויקט\\maven_data\\matrices"
+        # folder_with_commits_files = "C:\\My_Stuff\\BGU\\לימודים\\2021\\איתור תקלות\\פרויקט\\wicket_data\\matrices"
         files = listdir(folder_with_commits_files)
         commits = []
         for file in files:
             commits.append(file.split('_')[1])
 
+        repo_url = "https://github.com/apache/maven.git"
+        # repo_url = "https://github.com/apache/wicket.git"
+        tool = "C:\\Java_analysis\\java-callgraph-master\\target\\javacg-0.1-SNAPSHOT-static.jar"
+        java_exe = 'C:\\Program Files\\Java\\jdk1.8.0_291\\bin\\java.exe'
         for commit_id in commits:
-            repo_url = "https://github.com/apache/maven.git"
             extractor = MetricExtractor(repo_url, self.repo_path)
             repo = Repo(self.repo_path)
             self.assertTrue(not repo.bare)
-            # commit_id = "56cd921f"
             extractor.checkout(commit_id)
             commit = repo.head.commit
             self.assertTrue(commit_id in str(commit))
 
-            tool = "C:\\Java_analysis\\java-callgraph-master\\target\\javacg-0.1-SNAPSHOT-static.jar"
             raw_tool_file_extractor = JavaCallGraphMetricFileExtractorPlugin(tool)
             out_file = "{}.txt".format(commit_id)
             out_path = self.raw_metric_files_folder + os.path.sep + out_file
-            java_exe = 'C:\\Program Files\\Java\\jdk1.8.0_291\\bin\\java.exe'
             raw_tool_file_extractor.tool_generate_metric(java_exe, commit_id, self.repo_path, out_path)
 
     def test_wicket_repo_checkout_commit(self):
